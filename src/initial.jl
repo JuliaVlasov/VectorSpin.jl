e5609216564d29659246a38b677367b8db561f88
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2023-11-09 15:52:57 +0100

    Create initial.jl

---
+++ src/initial.jl
@@ -0,0 +1,30 @@
+export init
+
+"""
+$(SIGNATURES)
+
+Initialize the distribution function 
+"""
+function init(k, x, i, v1int, frequency, a, femi, tiK)
+
+    # initial Maxwellian
+    kk = 1.0
+    if femi ≈ 1
+        femi = 1
+    else
+        femi = 0.5
+    end
+
+    f =
+        (1 / sqrt(pi) / kk) *
+        exp(-(v1int[i])^2 / kk / kk) *
+        (1 + a * cos(frequency * x[k])) *
+        femi
+    df =
+        (1 / sqrt(pi) / kk) *
+        exp(-(v1int[i])^2 / kk / kk) *
+        (a * cos(frequency * x[k])) *
+        femi
+
+    return f, df
+end
