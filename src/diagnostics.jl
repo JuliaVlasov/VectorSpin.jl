380b7ef9f8d2c85c3bdaeb535e861723ffa0744f
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2023-11-11 21:53:03 +0100

    Update diagnostics.jl

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -1,100 +1,90 @@
-#=
 export diagnostics, Diagnostics, save!
 
 function diagnostics(f0, f2, f3, E1, E2, E3, A2, A3, M, N, L, H, h_int)
 
-value1 = 1:(M-1)÷2 .+ 1
-value2 = (M-1)÷2 .+ 2:M
+    dx = L / M
+    dv = 2H / N
+    k = fftfreq(M, M) .* 2pi ./ L
 
-B2value = zeros(ComplexF64, M)
-B3value = zeros(ComplexF64, M)
+    B2 = -1im * k .* A3
+    B3 = 1im * k .* A2
 
-B2value[value1] .= -((2pi * 1im / L * (value1 .- 1))) .* A3[value1]
-B2value[value2] .= -((2pi * 1im / L * (value2 .- 1 .- M))) .* A3[value2]
-B3value[value1] .= ((2pi * 1im / L * (value1 .- 1))) .* A2[value1]
-B3value[value2] .= ((2pi * 1im / L * (value2 .- 1 .- M))) .* A2[value2]
+    Tt = zeros(M)
+    ubar = zeros(M)
 
-EE1value = zeros(M)
-EE2value = zeros(M)
-EE3value = zeros(M)
-BB2value = zeros(M)
-BB3value = zeros(M)
-AA2value = zeros(M)
-AA3value = zeros(M)
-Tvaluet = zeros(M)
-uvaluebar = zeros(M)
+    EE1 = real(ifft(vec(E1)))
+    EE2 = real(ifft(vec(E2)))
+    EE3 = real(ifft(vec(E3)))
+    BB2 = real(ifft(vec(B2)))
+    BB3 = real(ifft(vec(B3)))
+    AA2 = real(ifft(vec(A2)))
+    AA3 = real(ifft(vec(A3)))
 
-EE1value .= real(ifft(vec(E1)))
-EE2value .= real(ifft(vec(E2)))
-EE3value .= real(ifft(vec(E3)))
-BB2value .= real(ifft(vec(B2value)))
-BB3value .= real(ifft(vec(B3value)))
-AA2value .= real(ifft(vec(A2)))
-AA3value .= real(ifft(vec(A3)))
+    # electric energy related to E1
+    Ex_energy = 1 / 2 * sum(EE1 .^ 2) * dx
+    # electric energy
+    E_energy =
+        1 / 2 * sum(EE1 .^ 2) * dx + 1 / 2 * sum(EE3 .^ 2) * dx + 1 / 2 * sum(EE2 .^ 2) * dx
+    # magnetic energy
+    B_energy = 1 / 2 * sum(BB2 .^ 2) * dx + 1 / 2 * sum(BB3 .^ 2) * dx
+    energy2 = E_energy + B_energy
 
-# electric energy related to E1
-Ex_energy = 1 / 2 * sum(EE1value[:] .^ 2) * L / M
-# electric energy
-E_energy =
-    1 / 2 * sum(EE1value[:] .^ 2) * L / M +
-    1 / 2 * sum(EE3value[:] .^ 2) * L / M +
-    1 / 2 * sum(EE2value[:] .^ 2) * L / M
-# magnetic energy
-B_energy = 1 / 2 * sum(BB2value[:] .^ 2) * L / M + 1 / 2 * sum(BB3value[:] .^ 2) * L / M
-energy2 = E_energy + B_energy
-v1node = (1:N) .* 2 .* H ./ N .- H .- H ./ N
+    v = collect(1:N) .* dv .- H .- H ./ N
 
-ff0value = zeros(N, M)
-ff2value = zeros(N, M)
-ff3value = zeros(N, M)
-for j = 1:M
-    Jvaluebar = 0
-    rhovaluebar = 0
-    for k = 1:N
-        ff0value[k, j] = 1 / 2 * (f0[k, j] * ((v1node[k]^2 + AA2value[j]^2 + AA3value[j]^2))) * L / M * 2 * H / N
-        ff2value[k, j] = h_int * f2[k, j] * (-BB2value[j]) * L / M * 2 * H / N
-        ff3value[k, j] = -h_int * f3[k, j] * (BB3value[j]) * L / M * 2 * H / N
-        Jvaluebar = Jvaluebar + f0[k, j] * v1node[k]
-        rhovaluebar = rhovaluebar + f0[k, j]
+
+    ff0 = zeros(N, M)
+    ff2 = zeros(N, M)
+    ff3 = zeros(N, M)
+    for j = 1:M
+        Jbar = 0
+        rhobar = 0
+        for k = 1:N
+            ff0[k, j] = 1 / 2 * (f0[k, j] * ((v[k]^2 + AA2[j]^2 + AA3[j]^2))) * dx * dv
+            ff2[k, j] = h_int * f2[k, j] * (-BB2[j]) * dx * dv
+            ff3[k, j] = -h_int * f3[k, j] * (BB3[j]) * dx * dv
+            Jbar = Jbar + f0[k, j] * v[k]
+            rhobar = rhobar + f0[k, j]
+        end
+        ubar[j] = Jbar / rhobar
     end
-    uvaluebar[j] = Jvaluebar / rhovaluebar
-end
-# temperature
-for j = 1:M, k = 1:N
-    Tvaluet[j] = Tvaluet[j] + f0[k, j] * ((v1node[k] - uvaluebar[j])^2) * 2 * H / N
-end
-energy1 = sum(ff0value .+ ff2value .+ ff3value)
-# total energy
-energy = energy1 + energy2
-# spectrum
-Sz = sum(f3) * L / M * (2 * H / N)
-return Ex_energy, E_energy, B_energy, energy, Sz, Tvaluet
+
+    # temperature
+    for j = 1:M, k = 1:N
+        Tt[j] = Tt[j] + f0[k, j] * ((v[k] - ubar[j])^2) * dv
+    end
+    energy1 = sum(ff0 .+ ff2 .+ ff3)
+    # total energy
+    energy = energy1 + energy2
+    # spectrum
+    Sz = sum(f3) * dx * dv
+    return Ex_energy, E_energy, B_energy, energy, Sz, Tt
+
 end
 
 struct Diagnostics
 
-    mesh :: Mesh
-    h_int :: Float64
-    Ex_energy :: Vector{Float64}
-    E_energy :: Vector{Float64}
-    B_energy :: Vector{Float64}
-    energy :: Vector{Float64}
-    Sz :: Vector{Float64}
-    Tvalue :: Vector{Vector{Float64}}
-    time :: Vector{Float64}
+    mesh::Mesh
+    h_int::Float64
+    Ex_energy::Vector{Float64}
+    E_energy::Vector{Float64}
+    B_energy::Vector{Float64}
+    energy::Vector{Float64}
+    Sz::Vector{Float64}
+    T::Vector{Vector{Float64}}
+    time::Vector{Float64}
 
-    function Diagnostics( f0, f2, f3, E1, E2, E3, A2, A3, mesh::Mesh, h_int)
+    function Diagnostics(f0, f2, f3, E1, E2, E3, A2, A3, mesh::Mesh, h_int)
 
-       results = diagnostics(f0, f2, f3, E1, E2, E3, A2, A3, mesh, h_int)
-       Ex_energy = [results[1]]
-       E_energy  = [results[2]]
-       B_energy  = [results[3]]
-       energy    = [results[4]]
-       Sz        = [results[5]]
-       Tvalue    = [results[6]]
-       time      = [0.0]
+        results = diagnostics(f0, f2, f3, E1, E2, E3, A2, A3, mesh, h_int)
+        Ex_energy = [results[1]]
+        E_energy = [results[2]]
+        B_energy = [results[3]]
+        energy = [results[4]]
+        Sz = [results[5]]
+        T = [results[6]]
+        time = [0.0]
 
-       new(mesh, h_int, Ex_energy, E_energy, B_energy, energy, Sz, Tvalue, time)
+        new(mesh, h_int, Ex_energy, E_energy, B_energy, energy, Sz, T, time)
 
     end
 
@@ -109,7 +99,7 @@ function save!(results, time, f0, f2, f3, E1, E2, E3, A2, A3)
     push!(results.B_energy, diags[3])
     push!(results.energy, diags[4])
     push!(results.Sz, diags[5])
-    push!(results.Tvalue, diags[6])
+    push!(results.T, diags[6])
     push!(results.time, time)
 
 end
@@ -143,16 +133,16 @@ function diagnostics(f0, f2, f3, E1, E2, E3, A2, A3, mesh::Mesh, h_int)
     B_energy = 1 / 2 * sum(BB2 .^ 2) * dx + 1 / 2 * sum(BB3 .^ 2) * dx
     energy2 = E_energy + B_energy
 
-    ff0 = 1 / 2 * (f0 .* (mesh.vnode.^2 .+ AA2'.^2 .+ AA3'.^2)) * dx * dv
+    ff0 = 1 / 2 * (f0 .* (mesh.vnode .^ 2 .+ AA2' .^ 2 .+ AA3' .^ 2)) * dx * dv
     ff2 = -h_int * f2 .* BB2' * dx * dv
     ff3 = -h_int * f3 .* BB3' * dx * dv
 
-    Jbar = vec(sum( f0 .* mesh.vnode, dims=1))
+    Jbar = vec(sum(f0 .* mesh.vnode, dims = 1))
     ubar = Jbar ./ sum(f0)
 
     # temperature
-    Tt = vec(sum(f0 .* (mesh.vnode .- ubar').^2, dims=1) .* dv)
-    energy1 = sum(sum(ff0 .+ ff2 .+ ff3, dims=1))
+    Tt = vec(sum(f0 .* (mesh.vnode .- ubar') .^ 2, dims = 1) .* dv)
+    energy1 = sum(sum(ff0 .+ ff2 .+ ff3, dims = 1))
     # total energy
     energy = energy1 + energy2
     # spectrum
@@ -162,37 +152,37 @@ end
 
 
 function kinetic_energy(f0, M, N, L, H)
-    v = (1:N) .* 2H / N .- H - H/N
+    v = (1:N) .* 2H / N .- H - H / N
     e = 0.0
-    for j=1:M, k=1:N
-       e += 1/2*(f0[k,j]*((v[k]^2)))*L/M*2H/N
+    for j = 1:M, k = 1:N
+        e += 1 / 2 * (f0[k, j] * ((v[k]^2))) * L / M * 2H / N
     end
     e
 end
 
-ex_energy( E1, L, M )  = 0.5 * sum(real(ifft(E1)).^2) * L/M
+ex_energy(E1, L, M) = 0.5 * sum(real(ifft(E1)) .^ 2) * L / M
 
-function bf_energy( f1, f2, f3, S1, S2, S3, M, N, L, H, tiK, n_i)
+function bf_energy(f1, f2, f3, S1, S2, S3, M, N, L, H, tiK, n_i)
 
-   bb1 = - K_xc * n_i * 0.5 * S1
-   bb2 = - K_xc * n_i * 0.5 * S2
-   bb3 = - K_xc * n_i * 0.5 * S3
+    bb1 = -K_xc * n_i * 0.5 * S1
+    bb2 = -K_xc * n_i * 0.5 * S2
+    bb3 = -K_xc * n_i * 0.5 * S3
 
-   ebf1 = 0.0
-   ebf2 = 0.0
-   ebf3 = 0.0
+    ebf1 = 0.0
+    ebf2 = 0.0
+    ebf3 = 0.0
 
-   for j=1:M, k=1:N
-       ebf1 += mub * f1[k,j] * bb1[j] * L / M * 2H / N
-       ebf2 += mub * f2[k,j] * bb2[j] * L / M * 2H / N
-       ebf3 += mub * f3[k,j] * bb3[j] * L / M * 2H / N
-   end
+    for j = 1:M, k = 1:N
+        ebf1 += mub * f1[k, j] * bb1[j] * L / M * 2H / N
+        ebf2 += mub * f2[k, j] * bb2[j] * L / M * 2H / N
+        ebf3 += mub * f3[k, j] * bb3[j] * L / M * 2H / N
+    end
 
-   ebf1, ebf2, ebf3
+    ebf1, ebf2, ebf3
 
 end
 
-function s_energy(S1, S2, S3, M, N, L, H, mub=0.3386, n_i = 1.0)
+function s_energy(S1, S2, S3, M, N, L, H, mub = 0.3386, n_i = 1.0)
 
     h_int = 2.0 * mub
     aj0 = 0.01475 * h_int / 2.0
@@ -203,7 +193,7 @@ function s_energy(S1, S2, S3, M, N, L, H, mub=0.3386, n_i = 1.0)
     S2t = fft(S2)
     S3t = fft(S3)
 
-    S1t .*= (1im * k) 
+    S1t .*= (1im * k)
     S2t .*= (1im * k)
     S3t .*= (1im * k)
 
@@ -211,14 +201,12 @@ function s_energy(S1, S2, S3, M, N, L, H, mub=0.3386, n_i = 1.0)
     ifft!(S2t)
     ifft!(S3t)
 
-    es1 = sum(aj0 .* n_i .* real(S1t).^2 * L / M)
-    es2 = sum(aj0 .* n_i .* real(S2t).^2 * L / M)
-    es3 = sum(aj0 .* n_i .* real(S3t).^2 * L / M)
+    es1 = sum(aj0 .* n_i .* real(S1t) .^ 2 * L / M)
+    es2 = sum(aj0 .* n_i .* real(S2t) .^ 2 * L / M)
+    es3 = sum(aj0 .* n_i .* real(S3t) .^ 2 * L / M)
 
     return es1, es2, es3
 
 end
 
-snorm(S1, S2, S3) = maximum(abs.((S1.^2 .+ S2.^2 .+ S3.^2) .- 1))
-
-=#
+snorm(S1, S2, S3) = maximum(abs.((S1 .^ 2 .+ S2 .^ 2 .+ S3 .^ 2) .- 1))
