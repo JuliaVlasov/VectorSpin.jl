10c8ce11b367812d0a6cd021ca4ae45e85a0ad7f
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2023-11-27 13:46:57 +0100

    Update initial.jl

--- examples/initial.jl
+++ examples/initial.jl
@@ -1,127 +1,53 @@
 using FFTW
+using Plots
+using ProgressMeter
+using TimerOutputs
 using VectorSpin
 
+const to = TimerOutput()
+
 # mesh and parameters 
-T = 5000 # final simulation time
-M = 119 # mesh number in x direction
-N = 129 # mesh number in v direction
-H = 10.0 # computational domain [-H/2,H/2] in v
-H = H / 2
-kkk = 0.5 # wave number/frequency
-L = 2pi / kkk # computational domain [0,L] in x
-tildeK = 0.1598 # normalized parameter tildeK
-h = 0.1 # time step size
-NUM = floor(Int, T / h + 1.1) # time step number
-x = collect(0:(M-1)) .* L ./ M # mesh in x
-v1 = collect(1:N) .* 2H ./ N .- H # mesh in v
+const T = 100 # final simulation time
+const M = 128 # mesh number in x direction
+const N = 128 # mesh number in v direction
+const H = 10.0 / 2 # computational domain [-H/2,H/2] in v
+const kkk = 0.5 # wave number/frequency
+const L = 2pi / kkk # computational domain [0,L] in x
+const tildeK = 0.1598 # normalized parameter tildeK
+const h = 0.1 # time step size
+const a = 0.001 # perturbation for f
 
-a = 0.001 # perturbation for f
-E1 = fft(-1.0 * a / kkk * sin.(kkk * x)) # electric field
-epsil = a # perturbation for S
-# spin variables
-S1 = zeros(M)
-S2 = zeros(M)
-S3 = zeros(M)
-
-dS1 = zeros(M)
-dS2 = zeros(M)
-dS3 = zeros(M)
-
-
-# initial value of S
-for k = 1:M
-    normx = sqrt(1.0 + epsil^2) #    norm of S
-    dS1[k] = epsil * cos(kkk * x[k]) / normx
-    dS2[k] = epsil * sin(kkk * x[k]) / normx
-    dS3[k] = 1.0 / normx - 1.0
-    S1[k] = dS1[k]
-    S2[k] = dS2[k]
-    S3[k] = 1.0 / normx
-end
-
-v1node = zeros(5N)
-for i = 1:N
-    v1node[5i-4] = v1[i] - 2H / N
-    v1node[5i-3] = v1[i] - (2H / N) * 3 / 4
-    v1node[5i-2] = v1[i] - (2H / N) * 1 / 2
-    v1node[5i-1] = v1[i] - (2H / N) * 1 / 4
-    v1node[5i] = v1[i]
-end
-
-f0_node = zeros(5N, M)
-df0_node = zeros(5N, M)
-f1_node = zeros(5N, M)
-f2_node = zeros(5N, M)
-f3_node = zeros(5N, M)
-df3_node = zeros(5N, M)
-femi1 = 1
-femi2 = -1
-
-"""
-initial Maxwellian
-"""
 function maxwellian(k, x, i, v1int, frequency, a, femi, tiK)
 
     kk = 1.0 # vth
+
     if femi == 1
         femi = 1
     else
         femi = 0.5
     end
-    value =
+
+    f =
         (1 / sqrt(pi) / kk) *
         exp(-(v1int[i])^2 / kk / kk) *
         (1 + a * cos(frequency * x[k])) *
         femi
-    valuedf =
+    df =
         (1 / sqrt(pi) / kk) *
         exp(-(v1int[i])^2 / kk / kk) *
         (a * cos(frequency * x[k])) *
         femi
 
-    return value, valuedf
+    return f, df
 end
 
-for k = 1:M, i = 1:5N
-    f0_node[i, k], df0_node[i, k] = maxwellian(k, x, i, v1node, kkk, a, femi1, tildeK)
-    f1_node[i, k] = 0.0
-    f2_node[i, k] = 0.0
-    f3_node[i, k], df3_node[i, k] = maxwellian(k, x, i, v1node, kkk, a, femi2, tildeK)
-end
-
-initialvalue_f0 = zeros(N, M)
-initialvalue_f1 = zeros(N, M)
-initialvalue_f2 = zeros(N, M)
-initialvalue_f3 = zeros(N, M)
-
-
-"""
-computation of integral average in each cell using newton-cotes formula
-"""
 function numeint(value, N)
-    integralvalue = 7 / 90 * value[1:5:5N-4] .+ 16 / 45 * value[2:5:5N-3]
-    integralvalue = integralvalue .+ 2 / 15 * value[3:5:5N-2]
-    integralvalue = integralvalue .+ 16 / 45 * value[4:5:5N-1]
-    integralvalue = integralvalue .+ 7 / 90 * value[5:5:5N]
-    integralvalue
+    integral = 7 / 90 * value[1:5:5N-4] .+ 16 / 45 * value[2:5:5N-3]
+    integral = integral .+ 2 / 15 * value[3:5:5N-2]
+    integral = integral .+ 16 / 45 * value[4:5:5N-1]
+    integral = integral .+ 7 / 90 * value[5:5:5N]
+    integral
 end
-
-
-# initial value of f
-
-for k = 1:M
-    initialvalue_f0[:, k] .= numeint(f0_node[:, k], N)
-    initialvalue_f1[:, k] .= numeint(f1_node[:, k], N)
-    initialvalue_f2[:, k] .= numeint(f2_node[:, k], N)
-    initialvalue_f3[:, k] .= numeint(f3_node[:, k], N)
-end
-
-f0 = initialvalue_f0
-f1 = initialvalue_f1
-f2 = initialvalue_f2
-f3 = initialvalue_f3
-
-
 
 function energy(f0, f1, f2, f3, E1, S1, S2, S3, M, N, L, H, tiK)
 
@@ -195,12 +121,107 @@ function energy(f0, f1, f2, f3, E1, S1, S2, S3, M, N, L, H, tiK)
 
 end
 
-for i = 2:NUM # run with time 
-    # Lie splitting
-    Hv!(f0, f1, f2, f3, E1, h, M, N, L, H)
-    He!(f0, f1, f2, f3, E1, h, H)
-    H1fh!(f0, f1, f2, f3, S1, S2, S3, h, M, N, L, H, tildeK)
-    H2fh!(f0, f1, f2, f3, S1, S2, S3, h, M, N, L, H, tildeK)
-    H3fh!(f0, f1, f2, f3, S1, S2, S3, h, M, N, L, H, tildeK)
-    @show energy(f0, f1, f2, f3, E1, S1, S2, S3, M, N, L, H, tildeK)
+function main()
+
+    NUM = floor(Int, T / h + 1.1) # time step number
+    x = collect(0:(M-1)) .* L ./ M # mesh in x
+    v1 = collect(1:N) .* 2H ./ N .- H # mesh in v
+    E1 = fft(-1.0 * a / kkk * sin.(kkk * x)) # electric field
+    epsil = a # perturbation for S
+
+    # spin variables
+    S1 = zeros(M)
+    S2 = zeros(M)
+    S3 = zeros(M)
+
+    dS1 = zeros(M)
+    dS2 = zeros(M)
+    dS3 = zeros(M)
+
+
+    # initial value of S
+    for k = 1:M
+        normx = sqrt(1.0 + epsil^2) #    norm of S
+        dS1[k] = epsil * cos(kkk * x[k]) / normx
+        dS2[k] = epsil * sin(kkk * x[k]) / normx
+        dS3[k] = 1.0 / normx - 1.0
+        S1[k] = dS1[k]
+        S2[k] = dS2[k]
+        S3[k] = 1.0 / normx
+    end
+
+    v1node = zeros(5N)
+    for i = 1:N
+        v1node[5i-4] = v1[i] - 2H / N
+        v1node[5i-3] = v1[i] - (2H / N) * 3 / 4
+        v1node[5i-2] = v1[i] - (2H / N) * 1 / 2
+        v1node[5i-1] = v1[i] - (2H / N) * 1 / 4
+        v1node[5i] = v1[i]
+    end
+
+    f0_node = zeros(5N, M)
+    df0_node = zeros(5N, M)
+    f1_node = zeros(5N, M)
+    f2_node = zeros(5N, M)
+    f3_node = zeros(5N, M)
+    df3_node = zeros(5N, M)
+    femi1 = 1
+    femi2 = -1
+
+
+    for k = 1:M, i = 1:5N
+        f0_node[i, k], df0_node[i, k] = maxwellian(k, x, i, v1node, kkk, a, femi1, tildeK)
+        f1_node[i, k] = 0.0
+        f2_node[i, k] = 0.0
+        f3_node[i, k], df3_node[i, k] = maxwellian(k, x, i, v1node, kkk, a, femi2, tildeK)
+    end
+
+    initialvalue_f0 = zeros(N, M)
+    initialvalue_f1 = zeros(N, M)
+    initialvalue_f2 = zeros(N, M)
+    initialvalue_f3 = zeros(N, M)
+
+
+
+
+    # initial value of f
+
+    for k = 1:M
+        initialvalue_f0[:, k] .= numeint(f0_node[:, k], N)
+        initialvalue_f1[:, k] .= numeint(f1_node[:, k], N)
+        initialvalue_f2[:, k] .= numeint(f2_node[:, k], N)
+        initialvalue_f3[:, k] .= numeint(f3_node[:, k], N)
+    end
+
+    f0 = initialvalue_f0
+    f1 = initialvalue_f1
+    f2 = initialvalue_f2
+    f3 = initialvalue_f3
+
+
+
+    t = Float64[]
+    e = Float64[]
+    push!(t, 0.0)
+    push!(e, energy(f0, f1, f2, f3, E1, S1, S2, S3, M, N, L, H, tildeK))
+
+    @showprogress 1 for i = 2:NUM # run with time 
+        # Lie splitting
+        @timeit to "Hv"   Hv!(f0, f1, f2, f3, E1, h, M, N, L, H)
+        @timeit to "He"   He!(f0, f1, f2, f3, E1, h, H)
+        @timeit to "H1fh" H1fh!(f0, f1, f2, f3, S1, S2, S3, h, M, N, L, H, tildeK)
+        @timeit to "H2fh" H2fh!(f0, f1, f2, f3, S1, S2, S3, h, M, N, L, H, tildeK)
+        @timeit to "H3fh" H3fh!(f0, f1, f2, f3, S1, S2, S3, h, M, N, L, H, tildeK)
+        push!(t, (i-1)*h)
+        push!(e, energy(f0, f1, f2, f3, E1, S1, S2, S3, M, N, L, H, tildeK))
+    end
+
+
+    return t, e
+
 end
+
+t, e = main()
+
+show(to)
+plot(t, log.(e))
